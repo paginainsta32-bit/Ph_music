6ed4cbffd7ad2ab8511847eb5b548e47fdcffa50
paginainsta32-bit <paginainsta32@gmail.com>
2026-07-31 08:06:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-﻿const CONFIG = {
+const CONFIG = {
 
-    // API do seu Worker
-    API_URL: "https://old-star-b1d3api-musicas.paginainsta32.workers.dev/",
+    // API do seu Worker (Substitua "SUA-SUBCONTA" pelo subdomínio real copiado da Cloudflare)
+    API_URL: "https://old-star-b1d3api-musicas.paginainsta32.workers.dev",
 
     // Bucket público do R2
-    R2_URL: "https://pub-dd2f575cd0624a87868bdcc44b319966.r2.dev/",
+    R2_URL: "https://pub-dd2f575cd0624a87868bdcc44b319966.r2.dev",
 
     // Capa padrão
     DEFAULT_COVER: "assets/capa-default.jpg"
